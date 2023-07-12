58446c68dc66be8b6f19e8dc1376a29153651d14
aksi1984 <pablo_xmen@op.pl>
2023-07-12 09:54:18 +0200

    BaseTest class implementation

---
+++ src/main/java/qa/base/BaseTest.java
@@ -0,0 +1,41 @@
+package qa.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import qa.driver.Driver;
+import qa.enums.Browser;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void startDriver() {
+
+        System.out.println("Starting driver");
+
+        driver = Driver.createDriver(Browser.CHROME);
+        driver.navigate().to("https://bazylika-pszow.pl");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    public void back() {
+
+        driver.navigate().back();
+    }
+
+    public WebDriver getDriver() {
+
+        return driver;
+    }
+
+    @AfterClass
+    protected void quitDriver() {
+
+        System.out.println("Quit driver");
+        //driver.quit();
+    }
+}
