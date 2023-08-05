37b8ab0bff96e33da6bb054e141be6d5de5f3519
aksi1984 <pablo_xmen@op.pl>
2023-08-05 23:12:25 +0200

    JSONReader class implementation

---
+++ src/main/java/qa/utils/JSONReader.java
@@ -0,0 +1,62 @@
+package qa.utils;
+
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JSONReader {
+
+    private static JSONObject jsonObject;
+
+    private static String fileToString(String filepath) {
+
+        try {
+
+            InputStream inputStream = Files.newInputStream(Paths.get(filepath));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line = bufferedReader.readLine();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while (line != null) {
+
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+
+            return stringBuilder.toString();
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void read(String filepath) {
+
+        jsonObject = new JSONObject(fileToString(filepath));
+    }
+
+    public static String[] get(String key, String node) {
+
+        Object object = jsonObject.get(key);
+        JSONObject jsonObject1 = (JSONObject) object;
+        JSONArray jsonArray = jsonObject1.getJSONArray(node);
+
+        String[] data = new String[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            data[i] = jsonArray.getString(i);
+        }
+
+        return data;
+    }
+}
