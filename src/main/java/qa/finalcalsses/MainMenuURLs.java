d2cb1a17d41375fefa84c4ed8135df5182b84d43
aksi1984 <pablo_xmen@op.pl>
2023-08-05 23:14:51 +0200

    MainMenuURLs class implementation

---
+++ src/main/java/qa/finalcalsses/MainMenuURLs.java
@@ -0,0 +1,14 @@
+package qa.finalcalsses;
+
+public final class MainMenuURLs {
+
+    public static final int PASTORAL_ANNOUNCEMENTS = 0;
+    public static final int MASS_INTENTIONS = 1;
+    public static final int FUNERALS = 2;
+    public static final int STEWARDS =  3;
+    public static final int PRIESTS = 4;
+    public static final int OFFICE = 5;
+    public static final int CONTACT = 6;
+    public static final int PARISH_SUPPORT = 7;
+    public static final int CONFESSION = 8;
+}
