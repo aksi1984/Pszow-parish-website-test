6348843d36f7d4586949d8ce1a144c0bad256256
aksi1984 <pablo_xmen@op.pl>
2023-08-06 21:53:56 +0200

    MiddleMenuURLs class implementation

---
+++ src/main/java/qa/finalcalsses/MiddleMenuURLs.java
@@ -0,0 +1,8 @@
+package qa.finalcalsses;
+
+public final class MiddleMenuURLs {
+
+    public static final int CAMERA = 0;
+    public static final int FACEBOOK = 1;
+    public static final int ARCHDIOCESE = 2;
+}
