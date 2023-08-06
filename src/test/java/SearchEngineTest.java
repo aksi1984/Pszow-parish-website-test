8d040ec8f5eabd4629fda25ed896c33d60b091f1
aksi1984 <pablo_xmen@op.pl>
2023-08-06 21:55:56 +0200

    Added check() method

--- src/test/java/SearchEngineTest.java
+++ src/test/java/SearchEngineTest.java
@@ -5,6 +5,9 @@ import qa.base.BaseTest;
 import qa.components.MainMenu;
 import qa.components.SearchEngine;
 import qa.components.SearchResultPage;
+import qa.utils.ExtentReportsManager;
+import qa.utils.Function_2;
+import qa.utils.Provider;
 
 public class SearchEngineTest extends BaseTest {
 
@@ -24,27 +27,37 @@ public class SearchEngineTest extends BaseTest {
     @Test(priority = 1)
     public void clicking() {
 
-        String phrase = "KANCELARIA";
+        ExtentReportsManager.setTestName("Clicking on the search engine field");
 
         searchEngine.clickOnField();
-        searchEngine.setPhrase(phrase);
 
-        Assert.assertEquals(searchEngine.getFieldValue(), phrase);
+        Assert.assertEquals(searchEngine.getFieldValue(), "");
     }
 
-    @Test(priority = 2)
-    public void correctPhrase() {
+    private void check(String[] data, Function_2 function) {
 
-        searchEngine.setPhrase("Kancelaria");
+        for (String phrase : data) {
 
-        Assert.assertTrue(searchResultPage.isContainerPageContentPresent());
+            searchEngine.setPhrase(phrase);
+            boolean state = searchResultPage.isContainerPageContentPresent();
+            back();
+            function.run(state);
+        }
     }
 
-    @Test(priority = 3)
-    public void incorrectPhrase() {
+    @Test(priority = 2, dataProvider = "correctPhrase", dataProviderClass = Provider.class)
+    public void correctPhrase(String[] data) {
 
-        searchEngine.setPhrase("bbbbbb");
+        ExtentReportsManager.setTestName("Searching with correct phrase");
 
-        Assert.assertFalse(searchResultPage.isContainerPageContentPresent());
+        check(data, (a)->{Assert.assertTrue(a);});
+    }
+
+    @Test(priority = 3, dataProvider = "incorrectPhrase", dataProviderClass = Provider.class)
+    public void incorrectPhrase(String[] data) {
+
+        ExtentReportsManager.setTestName("Searching with incorrect phrase");
+
+        check(data, (a)->{Assert.assertFalse(a);});
     }
 }
