7318fafd3f2f00b93199f473ec171cd0e451aa6a
aksi1984 <pablo_xmen@op.pl>
2023-08-24 00:04:20 +0200

    Added JavaScriptExecutor

--- src/main/java/qa/base/BasePage.java
+++ src/main/java/qa/base/BasePage.java
@@ -1,6 +1,8 @@
 package qa.base;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
@@ -9,23 +11,32 @@ public class BasePage {
 
     private final WebDriver driver;
 
+    private JavascriptExecutor javascriptExecutor;
+
     protected BasePage(WebDriver driver) {
 
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
+        javascriptExecutor = (JavascriptExecutor) driver;
     }
 
-    public WebDriver getDriver() {
+    protected WebDriver getDriver() {
 
         return driver;
     }
 
-    public String getURL() {
+    protected void clickElement(WebElement element) {
+
+        javascriptExecutor.executeScript("arguments[0].click();", element);
+    }
+
+    protected String getURL() {
 
         return driver.getCurrentUrl();
     }
 
-    public void wait(int seconds) {
+    protected void wait(int seconds) {
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
     }
