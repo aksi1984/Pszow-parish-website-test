b9565d0e3774f11c47e0d5bfff87e939e170f4f7
aksi1984 <pablo_xmen@op.pl>
2023-08-24 00:20:44 +0200

    ParishCommunity class implementation

---
+++ src/main/java/qa/pages/ParishCommunity.java
@@ -0,0 +1,26 @@
+package qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import qa.base.BasePage;
+
+import java.util.List;
+
+public class ParishCommunity extends BasePage {
+
+    public ParishCommunity(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@class='container pageContent clearfix']")
+    WebElement container;
+
+    public void click(int index) {
+
+        List<WebElement> elements = container.findElements(By.xpath("//img[@class]"));
+        clickElement(elements.get(index));
+    }
+}
