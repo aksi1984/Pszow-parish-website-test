0470ef644a5c979a60623d36dc89d796b92529e6
aksi1984 <pablo_xmen@op.pl>
2023-08-24 00:17:50 +0200

    MainMenuURLs class implementation

---
+++ src/main/java/qa/enums/MainMenuURLs.java
@@ -0,0 +1,14 @@
+package qa.enums;
+
+public enum MainMenuURLs {
+
+    PASTORAL_ANNOUNCEMENTS,
+    MASS_INTENTIONS,
+    FUNERALS,
+    STEWARDS,
+    PRIESTS,
+    OFFICE,
+    CONTACT,
+    PARISH_SUPPORT,
+    CONFESSION
+}
