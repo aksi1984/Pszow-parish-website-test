3629cfa1de2fd6a4bb0df3b1fe4b311eaa9815d6
aksi1984 <pablo_xmen@op.pl>
2023-08-24 00:24:08 +0200

    Removed class

--- src/main/java/qa/finalcalsses/MiddleMenuURLs.java
+++
@@ -1,8 +0,0 @@
-package qa.finalcalsses;
-
-public final class MiddleMenuURLs {
-
-    public static final int CAMERA = 0;
-    public static final int FACEBOOK = 1;
-    public static final int ARCHDIOCESE = 2;
-}
