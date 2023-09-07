2076699b3ed858ac888fdc8361946365b164f65e
aksi1984 <pablo_xmen@op.pl>
2023-09-07 22:00:57 +0200

    Added BasilicaInsidePageSteps class

---
+++ src/main/java/qa/stepclasses/BasilicaInsidePageSteps.java
@@ -0,0 +1,20 @@
+package qa.stepclasses;
+
+import io.qameta.allure.Step;
+import qa.pages.BasilicaInside;
+
+public class BasilicaInsidePageSteps {
+
+    private BasilicaInside basilicaInside;
+
+    public BasilicaInsidePageSteps(BasilicaInside basilicaInside) {
+
+        this.basilicaInside = basilicaInside;
+    }
+
+    @Step("Click the 'Prezbiterium' link")
+    public void clickThePresbyteryLink() {
+
+        basilicaInside.clickPresbyteryLink();
+    }
+}
