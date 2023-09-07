54f9d4ddaf16042b41288ec8540a4edcc365731b
aksi1984 <pablo_xmen@op.pl>
2023-09-07 20:51:05 +0200

    Added SearchEngineSteps class

---
+++ src/main/java/qa/stepclasses/SearchEngineSteps.java
@@ -0,0 +1,26 @@
+package qa.stepclasses;
+
+import io.qameta.allure.Step;
+import qa.components.SearchEngine;
+
+public class SearchEngineSteps {
+
+    private final SearchEngine searchEngine;
+
+    public SearchEngineSteps(SearchEngine searchEngine) {
+
+        this.searchEngine = searchEngine;
+    }
+
+    @Step("Enter a phrase")
+    public void enterPhrase(String phrase) {
+
+        searchEngine.setPhrase(phrase);
+    }
+
+    @Step("Press the 'Enter' key")
+    public void pressTheEnterKey() {
+
+        searchEngine.submit();
+    }
+}
