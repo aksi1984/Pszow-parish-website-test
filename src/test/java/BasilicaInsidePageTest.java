bc04552360ef91ed2ba4aa11165f73b02286fd88
aksi1984 <pablo_xmen@op.pl>
2023-10-05 17:15:24 +0200

    Added parameter

--- src/test/java/BasilicaInsidePageTest.java
+++ src/test/java/BasilicaInsidePageTest.java
@@ -1,15 +1,13 @@
 import io.qameta.allure.*;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.components.SideMenu;
 import qa.pages.BasilicaInside;
 import qa.stepclasses.BasilicaInsidePageSteps;
 import qa.utils.ExtentReportsManager;
-import qa.utils.JSONReader;
-import qa.enums.SideMenuURLs;
-
 import java.util.ArrayList;
 
 @Epic("Smoke tests")
@@ -17,31 +15,31 @@ import java.util.ArrayList;
 public class BasilicaInsidePageTest extends BaseTest {
 
     private BasilicaInsidePageSteps basilicaInsidePageSteps;
-    private String[] expectedResults;
-    @BeforeClass
-    public void init() {
+
+    @BeforeMethod
+    public void create() {
 
         basilicaInsidePageSteps = new BasilicaInsidePageSteps(new BasilicaInside(getDriver()));
-        expectedResults = JSONReader.get("URLs", "presbytery");
 
         SideMenu sideMenu = new SideMenu(getDriver());
-        sideMenu.click(SideMenuURLs.BASILICA_INSIDE.ordinal());
+        sideMenu.click("WNĘTRZE BAZYLIKI – PANORAMA");
     }
 
     @Test
+    @Parameters({"basilicaInsidePageURL"})
     @Severity(SeverityLevel.NORMAL)
     @Description("Test description: checking if the presbytery page opens after clicking on the 'Prezbiterium' link")
     @Story("Clicking the 'Prezbiterium link'")
-    public void presbyteryLink() {
+    public void presbyteryLink(String url) {
 
-        ExtentReportsManager.setTestName("\"PREZBITERIUM\" link");
+        //ExtentReportsManager.setTestName("\"PREZBITERIUM\" link");
 
         basilicaInsidePageSteps.clickThePresbyteryLink();
 
         ArrayList<String> tabs = new ArrayList<>(getDriver().getWindowHandles());
         getDriver().switchTo().window(tabs.get(tabs.size() - 1));
 
-        Assert.assertEquals(getDriver().getCurrentUrl(), expectedResults[0]);
+        Assert.assertEquals(getDriver().getCurrentUrl(), url);
     }
 
 
