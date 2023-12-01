e60d44d4c531923ce287b18f72ac18a4769576e5
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-01 07:02:48 +0100

    Updated the method body

--- src/main/java/qa/driver/WebDriverProvider.java
+++ src/main/java/qa/driver/WebDriverProvider.java
@@ -1,5 +1,34 @@
 package qa.driver;
 
+import org.reflections.Reflections;
+import qa.enums.Browser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
 public class WebDriverProvider {
 
+    public static WebDriverFactory getFactory(Browser browser) {
+
+        String packages = WebDriverFactory.class.getPackage().getName();
+        Reflections reflections = new Reflections(packages);
+
+        Set<Class<? extends WebDriverFactory>> factories = reflections.getSubTypesOf(WebDriverFactory.class);
+
+        Class<? extends WebDriverFactory> inherited = factories
+                .stream()
+                .filter(i -> i.getName().toLowerCase().contains(browser.getName().toLowerCase()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Could not find factory with name: " + browser.getName()));
+
+        String inheritedClassName = inherited.getName();
+
+        try {
+
+            return (WebDriverFactory) Class.forName(inheritedClassName).getConstructor().newInstance();
+        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException | NoSuchMethodException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
