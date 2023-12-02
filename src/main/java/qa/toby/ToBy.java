dda644c810e5efdcbccb0d6178c20de408ad0222
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-02 10:02:55 +0100

    Updated the get(WebElement) method body

--- src/main/java/qa/toby/ToBy.java
+++ src/main/java/qa/toby/ToBy.java
@@ -4,28 +4,35 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
-
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class ToBy {
 
-    private static String FOUND_BY = "foundBy";
+    private static final String FOUND_BY = "foundBy";
+    private static final String LOCATOR = "locator";
+    private static final String BY = "by";
 
-    public static void get(WebElement element) throws IllegalAccessException {
+    public static By get(WebElement element) throws IllegalAccessException {
 
         if (element instanceof Proxy proxy) {
-            
-            System.out.println("PROXY");
+
+            InvocationHandler invocationHandler = Proxy.getInvocationHandler(proxy);
+            Object locator = FieldUtils.readField(invocationHandler, LOCATOR, true);
+            Object by = FieldUtils.readField(locator, BY, true);
+
+            return FromString.get(by.toString());
 
         } else if (element instanceof RemoteWebElement remoteWebELement) {
 
             Object field = FieldUtils.readField(remoteWebELement, FOUND_BY, true);
             String[] split = field.toString().split("-> ");
 
+            return FromString.get(split[1]);
 
         } else {
 
-            System.out.println("OTHER");
+            return (By) FieldUtils.readField(element, BY, true);
         }
     }
 }
