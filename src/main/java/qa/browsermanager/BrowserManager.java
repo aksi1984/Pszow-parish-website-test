5eb5100de811866d20e7a7d6b4775b4a70009401
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-06 23:06:22 +0100

    Added Logger

--- src/main/java/qa/browsermanager/BrowserManager.java
+++ src/main/java/qa/browsermanager/BrowserManager.java
@@ -2,17 +2,20 @@ package qa.browsermanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BrowserManager {
 
     private static final String url = "https://bazylika-pszow.pl";
+    private static final Logger logger = LoggerFactory.getLogger(BrowserManager.class);
 
     public static void start(WebDriver driver) {
 
         String browserName = ((RemoteWebDriver) driver).getCapabilities().getBrowserName() +
                              " " + ((RemoteWebDriver) driver).getCapabilities().getBrowserVersion();
 
-        System.out.println("Opening the \"" + url + "\" website on the \"" + browserName + "\" browser");
+        logger.info("Opening the \"" + url + "\" website on the \"" + browserName + "\" browser");
 
         driver.navigate().to("https://bazylika-pszow.pl");
         driver.manage().window().maximize();
@@ -20,7 +23,7 @@ public class BrowserManager {
 
     public static void quit(WebDriver driver) {
 
-        System.out.println("Closing the \"" + url + "\" website");
+        logger.info("Closing the \"" + url + "\" website");
 
         driver.quit();
     }
