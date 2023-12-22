f3607f6b9215bf98c600bb92b64701e9ff2c284c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-22 23:43:58 +0100

    Added methods

--- src/test/java/SearchEngineTest.java
+++ src/test/java/SearchEngineTest.java
@@ -3,73 +3,73 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.components.MainMenu;
 import qa.components.SearchEngine;
-import qa.components.SearchResultPage;
-import qa.stepclasses.SearchEngineSteps;
+import qa.components.SearchResults;
+import qa.data.Phrase;
 import qa.dataprovider.Provider;
-import java.util.function.Consumer;
-import qa.extentreports.ExtentReportsManager;
+import qa.enums.URLs;
+import qa.stepclasses.SearchEngineSteps;
+
 
 @Epic("Smoke tests")
 @Feature("Search engine tests")
 public class SearchEngineTest extends BaseTest {
 
-    private SearchEngine searchEngine;
-    private SearchResultPage searchResultPage;
     private SearchEngineSteps searchEngineSteps;
+    private SearchResults searchResults;
 
     @BeforeMethod
-    public void create() throws IllegalAccessException {
+    public void create() {
 
-        MainMenu mainMenu = new MainMenu(getDriver());
-        searchEngine = new SearchEngine(getDriver());
-        searchResultPage = new SearchResultPage(getDriver());
-        searchEngineSteps = new SearchEngineSteps(searchEngine);
+        goToPage(URLs.HOME_PAGE.getName());
 
-        mainMenu.click("KONTAKT");
+        searchEngineSteps = new SearchEngineSteps(new SearchEngine(getPage()));
+        searchResults = new SearchResults(getPage());
+    }
+
+    private void fill(String phrase) {
+
+        searchEngineSteps.clickIcon();
+        searchEngineSteps.enterPhrase(phrase);
+        searchEngineSteps.pressTheEnterKey();
+    }
+    private int getNumberOfArticles() {
+
+        return getPage().querySelectorAll("[id^='post-']").size();
     }
 
     @Test(priority = 1)
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Testing whether the placeholder disappears when the field is clicked")
-    @Story("Clicking on the search engine field")
-    public void clickingOnField() throws IllegalAccessException {
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test description: testing whether the search field will be visible after clicking the search engine icon")
+    @Epic("Checking the search field visibility")
+    public void searchFieldVisibility() {
 
-        ExtentReportsManager.setTestName("Clicking on the search engine field");
+        searchEngineSteps.clickIcon();
 
-        searchEngine.clickOnField();
-
-        Assert.assertEquals(searchEngine.getFieldValue(), "", "Placeholder does not disappear when clicking the search box");
-    }
-
-    private void check(String phrase, Consumer<SearchResultPage> consumer) throws IllegalAccessException {
-
-        searchEngineSteps.enterPhrase(phrase);
-        searchEngineSteps.pressTheEnterKey();
-
-        consumer.accept(searchResultPage);
+        Assert.assertTrue(searchEngineSteps.getSearchEngine().getSearchField().isVisible());
     }
 
     @Test(priority = 2, dataProvider = "correctPhrase", dataProviderClass = Provider.class)
     @Severity(SeverityLevel.CRITICAL)
     @Description("Test description: testing whether articles will be found after searching with with a correct phrase")
     @Epic("Searching with a correct phrase")
-    public void correctPhrase(String phrase) throws IllegalAccessException {
+    public void correctPhrase(Phrase phrase) {
 
-        ExtentReportsManager.setTestName("Searching results with \"" + phrase + "\" as a correct phrase");
+        fill(phrase.getPhrase());
 
-        check(phrase, (SearchResultPage srp)->{  Assert.assertTrue(srp.isContainerPageContentPresent(), "No results after entering the correct phrase: \"" + phrase + "\""); });
+        Assert.assertTrue(getNumberOfArticles() > 0);
+        Assert.assertNotEquals(searchResults.getTextContent(), phrase.getMessage());
     }
 
     @Test(priority = 3, dataProvider = "incorrectPhrase", dataProviderClass = Provider.class)
     @Severity(SeverityLevel.CRITICAL)
     @Description("Test description: testing whether articles will be found after searching with with an incorrect phrase")
     @Epic("Searching with an incorrect phrase")
-    public void incorrectPhrase(String phrase) throws IllegalAccessException {
+    public void incorrectPhrase(Phrase phrase) {
 
-        ExtentReportsManager.setTestName("Searching results with \"" + phrase + "\" as an incorrect phrase");
+        fill(phrase.getPhrase());
 
-        check(phrase, (SearchResultPage srp)->{ Assert.assertFalse(srp.isContainerPageContentPresent(), "Results found after entering the incorrect phrase: \"" + phrase + "\""); });
+        Assert.assertFalse(getNumberOfArticles() > 0);
+        Assert.assertEquals(searchResults.getTextContent(), phrase.getMessage());
     }
 }
