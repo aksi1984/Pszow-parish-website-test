e967f192d6fe025a2669139a451f82f5474a99f5
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-22 09:50:33 +0100

    Added method

--- src/main/java/qa/jsonreader/JSONReader.java
+++ src/main/java/qa/jsonreader/JSONReader.java
@@ -3,14 +3,25 @@ package qa.jsonreader;
 import com.google.common.io.Resources;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import qa.data.Link;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
 
 public class JSONReader {
 
     private static JSONObject jsonObject;
+
+    private static JSONArray getJSONArray(String key, String node) {
+
+        Object object = jsonObject.get(key);
+        JSONObject jsonObject1 = (JSONObject) object;
+
+        return jsonObject1.getJSONArray(node);
+    }
 
     public static void read() throws IOException {
 
@@ -33,5 +44,17 @@ public class JSONReader {
         }
 
         return data;
+    }
+
+    public static Link[] getLinks(String node) {
+
+        JSONArray jsonArray = getJSONArray("links", node);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new Link(
+                        jsonArray.getJSONObject(i).getString("data"),
+                        jsonArray.getJSONObject(i).getString("url")
+                ))
+                .toArray(Link[]::new);
     }
 }
