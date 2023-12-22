3c9bb0d4d6691f27e32783aaebab428602545a22
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-22 23:43:21 +0100

    Renamed and removed unnecessary methods

---
+++ src/main/java/qa/components/SearchResults.java
@@ -0,0 +1,23 @@
+package qa.components;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import qa.base.BasePage;
+
+public class SearchResults extends BasePage {
+
+    private final Locator content;
+
+    public SearchResults(Page page) {
+
+        super(page);
+
+        content = page.locator("#content");
+    }
+
+    public String getTextContent() {
+
+        return content.textContent().trim();
+    }
+
+}
