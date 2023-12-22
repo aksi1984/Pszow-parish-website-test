e81ead15bfa26bfc73efeebe25a5d50f540a19e7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-22 09:53:27 +0100

    Removed unnecessary test methods

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,13 +1,12 @@
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import qa.base.BaseTest;
 import qa.components.MainMenu;
 import org.testng.annotations.Test;
+import qa.data.Link;
+import qa.dataprovider.Provider;
 import qa.stepclasses.MainMenuSteps;
-import qa.extentreports.ExtentReportsManager;
 import io.qameta.allure.*;
-import qa.utils.Message;
 
 @Epic("Smoke tests")
 @Feature("Main menu links tests")
@@ -22,120 +21,15 @@ public class MainMenuTest extends BaseTest {
         mainMenuSteps = new MainMenuSteps(mainMenu);
     }
 
-    @Test(priority = 1)
-    @Parameters({"pastoralURL"})
+    @Test(dataProvider = "mainMenu", dataProviderClass = Provider.class)
     @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the announcements page opens after clicking on the 'Ogłoszenia' link.")
-    @Story("Clicking the 'Ogłoszenia duszpasterskie' link")
-    public void pastoralAnnouncements(String url) throws IllegalAccessException {
+    @Description("Test description: checking if the announcements page opens after clicking the main menu link.")
+    @Story("Clicking the main menu link")
+    public void links(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setTestName("Clicking the \"OGŁOSZENIA DUSZPASTERSKIE\" link");
+        mainMenuSteps.clickLink(link.getData());
 
-        mainMenuSteps.clickThePastoraAnnouncementsLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 2)
-    @Parameters({"intentionsURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the mass intentions page opens after clicking on the 'Intencje mszalne' link.")
-    @Story("Clicking the 'Intencje mszalne' link")
-    public void massIntentions(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"INTENCJE MSZALNE\" link");
-
-        mainMenuSteps.clickTheMassIntentionsLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 3)
-    @Parameters({"funeralsURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the funerals page opens after clicking on the 'Pogrzeby' link.")
-    @Story("Clicking the 'Pogrzeby' link")
-    public void funerals(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"POGRZEBY\" link");
-
-        mainMenuSteps.clickTheFuneralsLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 4)
-    @Parameters({"stewardsURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the stevards page opens after clicking on the 'Szafarze' link.")
-    @Story("Clicking the 'Szafarze' link")
-    public void stewards(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"SZAFARZE\" link");
-
-        mainMenuSteps.clickTheStewardsLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 5)
-    @Parameters({"priestsURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the priests page opens after clicking on the 'Duszpasterze' link.")
-    @Story("Clicking the 'Duszpasterze' link")
-    public void priests(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"DUSZPASTERZE\" link");
-
-        mainMenuSteps.clickThePriestsLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 6)
-    @Parameters({"officeURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the office page opens after clicking on the 'Kancelaria' link.")
-    @Story("Clicking the 'Kancelaria' link")
-    public void office(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"KANCELARIA\" link");
-
-        mainMenuSteps.clickTheOfficeLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 7)
-    @Parameters({"contactURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the contact page opens after clicking on the 'Kontakt' link.")
-    @Story("Clicking the 'Kontakt' link")
-    public void contact(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"KONTAKT\" link");
-
-        mainMenuSteps.clickTheContactLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 8)
-    @Parameters({"supportURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the parish support page opens after clicking on the 'Wsparcie parafii' link.")
-    @Story("Clicking the 'Wsparcie parafii' link")
-    public void parishSupport(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"WSPARCIE PARAFII\" link");
-
-        mainMenuSteps.clickTheParishSupportLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
-    }
-
-    @Test(priority = 9)
-    @Parameters({"confessionURL"})
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test description: checking if the confession page opens after clicking on the 'Spowiedź w bazylice' link.")
-    @Story("Clicking the 'Spowiedź w bazylice' link")
-    public void confession(String url) throws IllegalAccessException {
-
-        ExtentReportsManager.setTestName("Clicking the \"SPOWIEDŻ W BAZYLICE\" link");
-
-        mainMenuSteps.clickTheConfessionLink();
-        Assert.assertEquals(getDriver().getCurrentUrl(), url, Message.getMessage(url));
+        Assert.assertEquals(getDriver().getCurrentUrl(), link.getUrl(),
+                "The page with address: " + link.getUrl() + " has not been opened");
     }
 }
