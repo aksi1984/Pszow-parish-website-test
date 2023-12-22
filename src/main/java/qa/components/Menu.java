dea18db014b4b0447d06696bf5f01122d010687f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-22 23:38:58 +0100

    Code rewritten

---
+++ src/main/java/qa/components/Menu.java
@@ -0,0 +1,17 @@
+package qa.components;
+
+import com.microsoft.playwright.Page;
+import qa.base.BasePage;
+
+public class Menu extends BasePage {
+
+    public Menu(Page page) {
+
+        super(page);
+    }
+
+    public void click(String id) throws IllegalAccessException {
+
+        getPage().locator("#" + id).click();
+    }
+}
