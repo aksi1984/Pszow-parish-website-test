70e18c089b8e934da6cb0fb4dc44890ccb4ed729
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-23 06:48:38 +0100

    Created the MediaIconsSteps class

---
+++ src/main/java/qa/stepclasses/MediaIconsSteps.java
@@ -0,0 +1,20 @@
+package qa.stepclasses;
+
+import io.qameta.allure.Step;
+import qa.components.Media;
+
+public class MediaIconsSteps {
+
+    private final Media media;
+
+    public MediaIconsSteps(Media media) {
+
+        this.media = media;
+    }
+
+    @Step("Click icon")
+    public void clickIcon(String text) {
+
+        media.clickIcon(text);
+    }
+}
