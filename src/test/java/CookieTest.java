64981db9ff7c2014254d63092293852253d15e5d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-24 14:37:55 +0100

    Code rewritten

--- src/test/java/CookieTest.java
+++ src/test/java/CookieTest.java
@@ -1,26 +1,25 @@
+import com.microsoft.playwright.options.Cookie;
 import io.qameta.allure.*;
-import org.openqa.selenium.Cookie;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.components.CookiesSection;
-import qa.extentreports.ExtentReportsManager;
-
-import java.util.Date;
-import java.util.Random;
-import java.util.Set;
-
+import qa.enums.URLs;
+import java.util.List;
 
 @Epic("Smoke tests")
 @Feature("Cookie tests")
 public class CookieTest extends BaseTest {
 
     @BeforeMethod
-    public void init() throws IllegalAccessException {
+    public void init() {
 
-        CookiesSection cookiesSection = new CookiesSection(getDriver());
-        cookiesSection.acceptCookies();
+        goToPage(URLs.HOME_PAGE.getName());
+    }
+
+    private List<Cookie> getCookies() {
+
+        return getBrowserContext().cookies();
     }
 
     @Test
@@ -29,38 +28,47 @@ public class CookieTest extends BaseTest {
     @Story("Checking the set of cookies size")
     public void setOfCookiesShouldNotBeEmpty() {
 
-        ExtentReportsManager.setTestName("Checking the set of cookies size");
-
-        Set<Cookie> cookies = getDriver().manage().getCookies();
-
-        Assert.assertFalse(cookies.isEmpty(), "The set is empty");
+        Assert.assertFalse(getCookies().isEmpty());
     }
 
     @Test
     @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: checking if the 'cookie_notice_accepted' cookie exist")
-    @Story("Checking the 'cookie_notice_accepted' cookie")
-    public void theCookieNoticeAcceptedCookieShouldExist() {
+    @Description("Test description: checking if the 'secure' value is not null")
+    @Story("Checking the 'cookie_notice_accepted' value")
+    public void theSecureValueShouldNotBeNull() {
 
-        ExtentReportsManager.setTestName("Checking the \"cookie_notice_accepted\" cookie");
-
-        Cookie cookie = getDriver().manage().getCookieNamed("cookie_notice_accepted");
-
-        Assert.assertEquals(cookie.getName(), "cookie_notice_accepted", "The \"cookie_notice_accepted\" cookie does not exist");
+        getCookies().forEach(cookie -> Assert.assertNotNull(cookie.secure));
     }
 
     @Test
     @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: checking if the 'cookie_notice_accepted' value is not null")
+    @Description("Test description: checking if the 'secure' value is true")
+    @Story("Checking the 'cookie_notice_accepted' value")
+    public void theSecureValueShouldBeTrue() {
+
+        getCookies().forEach(cookie -> Assert.assertTrue(cookie.secure));
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test description: checking if the 'expiry' value is not null")
     @Story("Checking the 'cookie_notice_accepted' value")
     public void theExpiryValueShouldNotBeNull() {
 
-        ExtentReportsManager.setTestName("Checking the \"cookie_notice_accepted\" value");
+        getCookies().forEach(cookie -> Assert.assertNotNull(cookie.expires));
+    }
 
-        Cookie cookie = getDriver().manage().getCookieNamed("cookie_notice_accepted");
-        String value = cookie.getValue();
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test description: checking if the 'expiry' value is in the future")
+    @Story("Checking the 'expiry' value")
+    public void theExpiryValueShouldBeInTheFuture() {
 
-        Assert.assertNotNull(value, "The \"cookie_notice_accepted\" value is null");
+        for (Cookie cookie : getCookies()) {
+
+            String cookieName = cookie.name;
+
+        }
     }
 
     @Test
@@ -69,83 +77,8 @@ public class CookieTest extends BaseTest {
     @Story("Checking the domain")
     public void theDomainShouldBeCorrect() {
 
-        ExtentReportsManager.setTestName("Checking the domain");
+        String expectedDomainName = "bazylika-pszow.pl";
 
-        Cookie cookie = getDriver().manage().getCookieNamed("cookie_notice_accepted");
-
-        Assert.assertEquals(cookie.getDomain(), "bazylika-pszow.pl", "Incorrect domain");
-    }
-
-    @Test
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: checking if a new cookie has the valid value and path")
-    @Story("Checking the newly created cookie")
-    public void theNewCookieShouldExist() {
-
-        ExtentReportsManager.setTestName("Checking the newly created cookie");
-
-        String name = "created_cookie";
-        String value = String.format("%d_", new Random().nextInt());
-        String path = "/";
-
-        Cookie cookie = new Cookie(name, value, path);
-        getDriver().manage().addCookie(cookie);
-
-        Set<Cookie> cookies = getDriver().manage().getCookies();
-
-        Assert.assertTrue(cookies.contains(cookie));
-
-        Cookie given = getDriver().manage().getCookieNamed(name);
-        Assert.assertEquals(given.getValue(), value, "Incorrect the new cookie value");
-        Assert.assertEquals(given.getPath(), path, "Incorrect the new cookie path");
-    }
-
-    @Test
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: Check if there is a new cookie with a date in the past")
-    @Story("Creating a cookie with a date in the past")
-    public void expiryInThePast() {
-
-        ExtentReportsManager.setTestName("Creating a cookie with a date in the past");
-
-        String name = "bad_cookie";
-        String value = String.format("exp_%d_", new Random().nextInt());
-        String path = "/";
-        Date expiry = new Date(System.currentTimeMillis() - 100000);
-
-        Cookie cookie = new Cookie(name, value, path, expiry);
-        getDriver().manage().addCookie(cookie);
-
-        Set<Cookie> cookies = getDriver().manage().getCookies();
-
-        Assert.assertFalse(cookies.contains(cookie), "A new cookie with a date in the past exists");
-    }
-
-    @Test
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: Checking whether cookies exist after deleting them")
-    @Story("Deleting all cookies")
-    public void deleteAllCookies() {
-
-        ExtentReportsManager.setTestName("Deleting all cookies");
-
-        getDriver().manage().deleteAllCookies();
-
-        Set<Cookie> cookies = getDriver().manage().getCookies();
-
-        Assert.assertTrue(cookies.isEmpty(), "The cookie exists");
-    }
-
-    @Test
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Test description: Checking if a cookie has been deleted by name")
-    @Story("Deleting a cookie by name")
-    public void deleteCookieByItsName() {
-
-        ExtentReportsManager.setTestName("Deleting a cookie by name");
-
-        getDriver().manage().deleteCookieNamed("PHPSESSID");
-
-        Assert.assertNull(getDriver().manage().getCookieNamed("PHPSESSID"), "The cookie has not been deleted");
+        getCookies().forEach(c -> Assert.assertEquals(c.domain, expectedDomainName));
     }
 }
