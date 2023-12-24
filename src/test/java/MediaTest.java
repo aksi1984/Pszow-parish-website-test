82d1f9298f2ee7f2a11b64b70243e3c15b060a3a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-24 07:18:50 +0100

    Created tests for media icons

---
+++ src/test/java/MediaTest.java
@@ -0,0 +1,46 @@
+import com.microsoft.playwright.Page;
+import io.qameta.allure.*;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import qa.base.BaseTest;
+import qa.components.Media;
+import qa.data.Link;
+import qa.dataprovider.Provider;
+import qa.enums.URLs;
+import qa.stepclasses.MediaIconsSteps;
+
+
+@Epic("Smoke tests")
+@Feature("Middle menu links test")
+public class MediaTest extends BaseTest {
+
+    private MediaIconsSteps mediaIconsSteps;
+
+    @BeforeMethod
+    public void create() {
+
+        goToPage(URLs.HOME_PAGE.getName());
+
+        mediaIconsSteps = new MediaIconsSteps(new Media(getPage()));
+    }
+
+    private void check(Link link) {
+
+        Page page = getPage().waitForPopup(() -> {
+            mediaIconsSteps.clickIcon(link.getData());
+        });
+
+        Assert.assertTrue(page.url().contains(link.getUrl()));
+    }
+
+    @Test(dataProvider = "media", dataProviderClass = Provider.class)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test description: checking if the page opens after clicking on the media icon.")
+    @Story("Clicking the media icon")
+    public void icon(Link link) {
+
+        check(link);
+    }
+}
