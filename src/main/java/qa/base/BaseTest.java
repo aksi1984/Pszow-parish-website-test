5cad98704889e38dab9183bc9c399b983be5fe53
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-24 11:49:06 +0100

    Updated imports

--- src/main/java/qa/base/BaseTest.java
+++ src/main/java/qa/base/BaseTest.java
@@ -4,8 +4,8 @@ import com.microsoft.playwright.Page;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import qa.driver.PlaywrightBrowserLauncher;
-import qa.driver.PlaywrightProvider;
+import qa.playwright.PlaywrightBrowserLauncher;
+import qa.playwright.PlaywrightProvider;
 import qa.enums.BrowserType;
 import qa.jsonreader.JSONReader;
 
